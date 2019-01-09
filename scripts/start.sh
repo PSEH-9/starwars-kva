000aa20ee104f55692b2837002e5cb53c84eaeb6
Karan Arora <karora2@sapient.com>
2019-01-09 12:44:04 +0530

    Added start stop scripts

---
+++ scripts/start.sh
@@ -0,0 +1,2 @@
+nohup java -jar /home/ubuntu/kva/starwars-kva-0.0.1-SNAPSHOT.jar --server.port=8080 > /home/ubuntu/kva/start.log &
+exit 0
---
+++ scripts/stop.sh
@@ -0,0 +1,2 @@
+pkill -f starwars-kva-0.0.1-SNAPSHOT.jar
+exit 0
